7badabe75c8f4a9a87906080b7776407c6df955b
jarbasai <jarbasai@mailfence.com>
2021-12-02 12:09:43 +0000

    :tada:

---
+++ gruut_plugin/__init__.py
@@ -0,0 +1,11 @@
+from ovos_plugin_manager.templates.phonemes import Grapheme2PhonemePlugin
+from gruut import sentences
+
+
+class GruutPlugin(Grapheme2PhonemePlugin):
+
+    def get_ipa(self, word, lang):
+        for sent in sentences(word, lang=lang):
+            for word in sent:
+                if word.phonemes:
+                    return [p.replace('ˈ', "") for p in word.phonemes]
---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+from gruut_plugin import GruutPlugin
+
+print(GruutPlugin().guess_ipa_word("mycroft", "en"))
+print(GruutPlugin().guess_arpa("ok google", "en"))
